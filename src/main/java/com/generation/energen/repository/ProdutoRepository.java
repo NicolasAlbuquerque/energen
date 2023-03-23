abb79a6ca704d3acf37a567d19bdefc9ddb1d9d6
Fabio Martins <generation@mail.com>
2023-03-23 11:08:18 -0300

    Atividade 5

--- src/main/java/com/generation/energen/repository/ProdutoRepository.java
+++ src/main/java/com/generation/energen/repository/ProdutoRepository.java
@@ -1,11 +1,18 @@
 package com.generation.energen.repository;
 
 import com.generation.energen.model.Produto;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
+	public List<Produto> findAllByNomeContainingIgnoreCase (@Param("nome") String nome);
+	
+	public List<Produto> findAllByMarcaContainingIgnoreCase (@Param("marca") String marca);
 
 }
